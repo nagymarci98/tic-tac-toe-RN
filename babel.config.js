6ecd0a9b0d38d6a176e0e3c8a19ad1114e764547
Nagy Márton <55578294+nagymarci98@users.noreply.github.com>
2022-03-30 14:56:00 +0200

    Added module resolver to the project, and added aliases to the config files. Also, created the procject structure.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      "module-resolver", {
+        alias: {
+          "@screens": "./src/screens",
+          "@components": "./src/components",
+          "@config": "./src/config",
+          "@utils": "./src/utils",
+          "@hooks": "./src/hooks",
+          "@store": "./src/store",
+          "@models": "./src/models",
+          "@assets": "./assets",
+        }
+      }
+    ]
+  ]
 };
