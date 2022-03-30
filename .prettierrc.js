cda4bfb48b64c0d0dadf05cf5ac04a1cb7a2d069
Nagy Márton <55578294+nagymarci98@users.noreply.github.com>
2022-03-30 11:51:16 +0200

    Addede prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,30 @@
 module.exports = {
-  bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
+  semi: false,
+  tabWidth: 2,
+  bracketSameLine: false,
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
+  requirePragma: false,
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+  endOfLine: "auto",
+  /* importOrder: [
+    "^react(.*)$", "^@config/(.*)$", "^@utils/(.*)$", "^@hooks/(.*)$",
+    "^@models/(.*)$", "^@store/(.*)$", "^@components/(.*)$", "^@screens/(.*)$", "^[./]"
+  ],
+  importOrderSeparation: trueimportOrder: [
+    "^react(.*)$", "^@config/(.*)$", "^@utils/(.*)$", "^@hooks/(.*)$",
+    "^@models/(.*)$", "^@store/(.*)$", "^@components/(.*)$", "^@screens/(.*)$", "^[./]"
+  ],
+  importOrderSeparation: true */
 };
